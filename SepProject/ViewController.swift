7076a463cbdb187ba1b2b9c53ae331f84698bfcf
Naveen Chowdary <naveenchowdary@naveens-macbook-air.local>
2018-09-11 16:10:20 +0530

    Initial Commit

---
+++ SepProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SepProject
+//
+//  Created by Naveen Chowdary on 11/09/18.
+//  Copyright © 2018 Naveen Chowdary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
